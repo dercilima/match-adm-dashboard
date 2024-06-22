d4cabfb4b20c49fad9ef28977268465b0f47aaef
Derci Santos <dercilima.si@gmail.com>
2024-06-22 11:04:21 -0300

    Import de módulos necessários para a tela de Login

--- src/app/components/auth/auth.module.ts
+++ src/app/components/auth/auth.module.ts
@@ -2,11 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PasswordModule } from 'primeng/password';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
-  declarations: [
-    LoginComponent
+  declarations: [LoginComponent],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    PasswordModule,
+    CheckboxModule,
+    ButtonModule
   ],
-  imports: [CommonModule, AuthRoutingModule],
 })
 export class AuthModule {}
