e837e71c3578c63b88f38ad488ecc4e1d251c4e5
Derci Santos <dercilima.si@gmail.com>
2024-06-22 09:55:07 -0300

    Criado o diretório components e movido o HomeComponent.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
 import { AppLayoutModule } from './layout/app.layout.module';
 
 @NgModule({
